b715af36e76fc977d0172c05a20668ceb92d46ee
Max Karpinski <mkarpins@hamilton.edu>
2026-01-29 16:32:44 +0000

    Start in-class questions

--- EVNST_A1.R
+++ EVNST_A1.R
@@ -1,1 +1,34 @@
-# this is an R script yeah 
+# Homework 1: Intro to R & GitHub ----
+#Max Karpinski
+#ENVST 325: Environmental Data Science, Hamilton College
+# In-class prompts ----
+# R does calculations
+2+10
+50*4
+
+# Create a variable 
+aNumber = 23452346
+
+aNumber*5
+#ADK in feet
+peaks = c(5344,5114,4960)
+#covert to meters
+peaks/3.281
+#Prominence in ft
+prom = c(4914,2100,840)
+
+peaks-prom
+
+#peak names
+peakNames = c("Mount Marcy", "Algonquin Peak", "Mount Haystack")
+
+#Set up data frame
+highPeaks = data.frame(elev = peaks, prom = prom, name = peakNames)
+#Show elevation column 
+highPeaks$elev
+highPeaks[1,]
+
+# In-class Prompts Questions ----
+
+
+# Homework ----
